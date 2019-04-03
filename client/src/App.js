dab2f378160e14f31a30f710c474a308f9cbd787
Jen Pirrone <pirronej104@gmail.com>
2019-04-03 18:53:40 -0400

    duh

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,23 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Books from "./pages/Books";
-import Nav from "./components/Nav";
 import Detail from "./pages/Detail";
-import { BrowserRouter as Router, Route} from "react-router-dom"
+import NoMatch from "./pages/NoMatch";
+import Nav from "./components/Nav";
 
 function App() {
   return (
-    <div>
-      <Nav />
-      <switch>
-        <Router>
+    <Router>
+      <div>
+        <Nav />
+        <Switch>
           <Route exact path="/" component={Books} />
           <Route exact path="/books" component={Books} />
-          <Route exact path="/books/: id" component={Detail} />
-        </Router>
-      </switch>
-      <Books />
-    </div>
+          <Route exact path="/books/:id" component={Detail} />
+          <Route component={NoMatch} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -2,15 +2,19 @@ import React, { Component } from "react";
 import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../components/Grid";
 import Jumbotron from "../components/Jumbotron";
-
+import API from "../utils/API";
 
 class Detail extends Component {
   state = {
     book: {}
   };
-  // Add code to get the book with an _id equal to the id in the route param
-  // e.g. http://localhost:3000/books/:id
-  // The book id for this route can be accessed using this.props.match.params.id
+  // When this component mounts, grab the book with the _id of this.props.match.params.id
+  // e.g. localhost:3000/books/599dcb67f0f16317844583fc
+  componentDidMount() {
+    API.getBook(this.props.match.params.id)
+      .then(res => this.setState({ book: res.data }))
+      .catch(err => console.log(err));
+  }
 
   render() {
     return (
@@ -28,7 +32,9 @@ class Detail extends Component {
           <Col size="md-10 md-offset-1">
             <article>
               <h1>Synopsis</h1>
-              <p>{this.state.book.synopsis}</p>
+              <p>
+                {this.state.book.synopsis}
+              </p>
             </article>
           </Col>
         </Row>
